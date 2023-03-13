241cd284e069b3060e1fad9008b38c836fbccfaa
jyoung7988 <jyoung7988@naver.com>
2023-03-13 12:57:52 +0900

    git init

---
+++ src/test/java/querydsl/stydy/StydyApplicationTests.java
@@ -0,0 +1,13 @@
+package querydsl.stydy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StydyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
