5f46b2f6975ba9c00b1003a18ce703763e3310f1
jyoung7988 <jyoung7988@naver.com>
2023-03-13 13:34:14 +0900

    where and 조건을 파라미터로 전달 가능

--- src/test/java/querydsl/stydy/entity/QuerydslBasicTest.java
+++ src/test/java/querydsl/stydy/entity/QuerydslBasicTest.java
@@ -10,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static querydsl.stydy.entity.QMember.*;
 
@@ -93,5 +95,25 @@ public class QuerydslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
+    @Test
+    public void search(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchAndParam() {
+        List<Member> result1 = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"),
+                        member.age.eq(10))
+                .fetch();
+        assertThat(result1.size()).isEqualTo(1);
+    }
 
 }
