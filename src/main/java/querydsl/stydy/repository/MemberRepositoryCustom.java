080caf969955d65dd0eb59447767c415f627de6d
jyoung7988 <jyoung7988@naver.com>
2023-03-14 13:40:21 +0900

    페이징 - 전체 카운트를 한번에 조회 fetchResults 사용 하지만 지금은 fetchResults is deprecated 이다.

--- src/main/java/querydsl/stydy/repository/MemberRepositoryCustom.java
+++ src/main/java/querydsl/stydy/repository/MemberRepositoryCustom.java
@@ -1,5 +1,7 @@
 package querydsl.stydy.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import querydsl.stydy.dto.MemberSearchCondition;
 import querydsl.stydy.dto.MemberTeamDto;
 
@@ -7,4 +9,9 @@ import java.util.List;
 
 public interface MemberRepositoryCustom {
     List<MemberTeamDto> search(MemberSearchCondition condition);
+
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition,
+                                         Pageable pageable);
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition,
+                                          Pageable pageable);
 }
